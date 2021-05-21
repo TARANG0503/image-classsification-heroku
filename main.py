a5a6b55b3cc6eaa43fe499b6900567c943538189
TARANG0503 <40140984+TARANG0503@users.noreply.github.com>
2021-05-21 21:21:01 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request
+
+import tensorflow as tf
+from keras.preprocessing.image import load_img,img_to_array
+from keras.applications.densenet import DenseNet121
+from keras.applications.densenet import preprocess_input, decode_predictions
+
+
+app=Flask(__name__)
+model=DenseNet121(weights='imagenet', include_top=True)
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    return render_template('index.html')
+
+@tf.function
+@app.route('/',methods=['POST'])
+def predict():
+    imagefile=request.files['image_file']
+    image_path="./images/"+imagefile.filename
+    imagefile.save(image_path)
+
+    
+    image=load_img(image_path,target_size=(224,224))
+    image=img_to_array(image)
+    image=image.reshape((1,image.shape[0],image.shape[1],image.shape[2]))
+    image=preprocess_input(image)
+    ypred=model.predict(image)
+    label=decode_predictions(ypred)
+    label=label[0][0]
+    classification='%s (%.2f%%)' %(label[1],label[2]*100)
+    return render_template('index.html',prediction=classification)
+
+if __name__=='__main__':
+    app.run(port=3000,debug=True)
